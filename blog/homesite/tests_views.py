ca18aae352c7898cf137ddb3b61725249e381292
Leandro Barbosa <leandrobar93@gmail.com>
2015-05-23 18:20:32 -0300

    update views

--- blog/homesite/tests_views.py
+++ blog/homesite/tests_views.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 from django.test import TestCase
 
-from homesite.views import HomeView
+from .views import HomeView
+from .models import Post
 
 
 class HomeViewTest(TestCase):
@@ -15,3 +16,14 @@ class HomeViewTest(TestCase):
     def test_get_context_data(self):
         esperado = self.view.get_context_data()
         self.assertEqual(esperado.keys(), ['posts', 'view'])
+
+    def test_get_context_data_query_vazia(self):
+        resposta = list(self.view.get_context_data()['posts'])
+        esperado = list(Post.objects.all())
+        self.assertEqual(resposta, esperado)
+
+    def test_get_context_data_com_objeto(self):
+        Post.objects.create(titulo='Foo', descricao='bar')
+        esperado = Post.objects.all()
+        resposta = self.view.get_context_data()['posts']
+        self.assertEqual(resposta[0].titulo, esperado[0].titulo)
--- blog/homesite/views.py
+++ blog/homesite/views.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from django.views.generic import TemplateView
 
+from .models import Post
+
 
 class HomeView(TemplateView):
     template_name = 'homesite/base.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['posts'] = 'FOO'
+        context['posts'] = Post.objects.all()
         return context
